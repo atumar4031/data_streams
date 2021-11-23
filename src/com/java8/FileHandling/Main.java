43325958b59b9e4274a7cc9f755bc559c69230ae
Almustapha <almusataphatukur111@gmail.com>
2021-11-23 11:08:28 +0100

    Complete CRUID using file system and stream API in java

--- src/com/java8/FileHandling/Main.java
+++ src/com/java8/FileHandling/Main.java
@@ -1,10 +1,8 @@
 package com.java8.FileHandling;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.security.cert.Extension;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -66,10 +64,12 @@ public class Main {
                 case 2 -> {
                     if(file.isFile()){
                         try{
-                            oinputs = new ObjectInputStream(new FileInputStream(file));
-                            people = (ArrayList<Person>) oinputs.readObject();
-                            oinputs.close();
+//                            oinputs = new ObjectInputStream(new FileInputStream(file));
+//                            people = (ArrayList<Person>) oinputs.readObject();
+//                            oinputs.close();
+                            System.out.println("--------------------------------------------");
                             people.stream().forEach(System.out::println);
+                            System.out.println("--------------------------------------------");
 
                         }catch (Exception e){
                             e.printStackTrace();
@@ -122,14 +122,90 @@ public class Main {
                     System.out.println("--------------------------------------------");
                 }
                 case 6 -> {
+                    System.out.println("user id: ");
+                    int uid = scan2.nextInt();
+                    var result = people.stream()
+                            .map(person -> person.getId())
+                            .filter(p -> p.equals(uid)).findFirst().orElse(0);
+                    if (result != 0){
+                        System.out.println("Name: ");
+                        String name = scan1.nextLine();
+                        people.stream()
+                                .map(person -> {
+                                    if(person.getId() == uid){
+                                        person.setName(name);
+                                    }
+                                    return person;
+                                }).forEach(System.out::println);
+                    }else
+                        System.out.println("Record not found");
+                }
+                case 7 -> {
+                    System.out.println("user id: ");
+                    int uid = scan2.nextInt();
+                    var result = people.stream()
+                            .map(person -> person.getId())
+                            .filter(p -> p.equals(uid)).findFirst().orElse(0);
+                    if (result != 0){
+                        System.out.println("Name: ");
+                        String name = scan1.nextLine();
+                        ArrayList<Person> p = (ArrayList<Person>)people.stream()
+                                .map(person -> {
+                                    if (person.getId() == uid) {
+                                        person.setName(name);
+                                    }
+                                    return person;
+                                }).collect(Collectors.toList());
+                        try{
+                            oouts = new ObjectOutputStream(new FileOutputStream(file));
+                            oouts.writeObject(p);
+                            oouts.close();
+                            System.out.println("Record updated");
+                        }catch(Exception ex){
+                            ex.printStackTrace();
+                        }
+                    }else
+                        System.out.println("Record not found");
+                }
+                case 8 -> {
                     System.out.println("Delete id: ");
                     int uid = scan2.nextInt();
-                    List<Person> lp = people.stream()
-                            .filter(person -> person.getId() != uid)
-                            .collect(Collectors.toList());
-                 // break point-------------------------------------------------->>>>>>>>>>>>>>>>>
+                    LinkedList<Person> linked_person = people.stream().collect(Collectors.toCollection(LinkedList::new));
+                    Iterator it = linked_person.iterator();
+                    while(it.hasNext()){
+                        var value = (Person) it.next();
+                        if(value.getId() == uid){
+                            it.remove();
+                            people = linked_person.stream().collect(Collectors.toCollection(ArrayList::new));
+                            break;
+                        }
+                    }
+                    System.out.println("""
+                            Data deleted successfully
+                            """);
+
                 }
-                case 7 -> System.out.println("Delete user");
+                case 9 -> {
+                    System.out.println("Delete id: ");
+                    int uid = scan2.nextInt();
+                    LinkedList<Person> linked_person = people.stream().collect(Collectors.toCollection(LinkedList::new));
+                    Iterator it = linked_person.iterator();
+                    while(it.hasNext()){
+                        var value = (Person) it.next();
+                        if(value.getId() == uid){
+                            it.remove();
+                            people = linked_person.stream().collect(Collectors.toCollection(ArrayList::new));
+                            break;
+                        }
+                    }
+                    oouts = new ObjectOutputStream(new FileOutputStream(file));
+                    oouts.writeObject(people);
+                    oouts.close();
+                    System.out.println("""
+                            Data deleted successfully
+                            """);
+
+                }
             }
 
         }while (choice != 99);
@@ -144,8 +220,10 @@ public class Main {
         System.out.println("3. Search Users");
         System.out.println("4. Sort Users from UI");
         System.out.println("5. Sort Users from file");
-        System.out.println("6. Delete Users from UI");
-        System.out.println("7. Delete Users from file");
+        System.out.println("6. Update Users from UI");
+        System.out.println("7. Update Users from file");
+        System.out.println("8. Delete Users from UI");
+        System.out.println("9. Delete Users from file");
         return scan.nextInt();
 
     }
